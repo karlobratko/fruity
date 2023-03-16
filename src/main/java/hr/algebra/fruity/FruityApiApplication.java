4bcb1d1f6387e95c5cba954a26c4593b0434d7a7
Karlo Bratko <karlobratko@gmail.com>
2023-03-16 16:27:28 +0100

    Initial commit

---
+++ src/main/java/hr/algebra/fruity/FruityApiApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.fruity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruityApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FruityApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/hr/algebra/fruity/FruityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.fruity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruityApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
